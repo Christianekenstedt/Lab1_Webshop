931f567f96e78b0c8594e43486af979243031571
christianekenstedt <christian>
2016-09-28 16:35:41 +0200

    Order almost done.

--- src/DAL/OrderDB.java
+++ src/DAL/OrderDB.java
@@ -8,7 +8,7 @@ import java.sql.*;
 import java.util.Vector;
 
 /**
- * Created by chris on 2016-09-28.
+ * Created by Christian Ekenstedt on 2016-09-28.
  */
 public class OrderDB extends Order{
 
@@ -16,11 +16,73 @@ public class OrderDB extends Order{
         super(id,owner,status);
     }
     public static Order getOrderFromDB(int id) {
+        PreparedStatement stmt = null;
+        PreparedStatement userStmt = null;
+
+        Connection con = DBManager.getConnection();
+
+        String query = "SELECT * FROM ShopOrder WHERE id = ?";
+
+        try {
+            stmt.setInt(1,id);
+            stmt = con.prepareStatement(query);
+            ResultSet rs = stmt.executeQuery();
+
+            while (rs.next()){
+                //TODO: Catch the user.
+                return new OrderDB(rs.getInt("id"),null,rs.getInt("status"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+
+        }finally {
+            if (stmt!=null) try {
+                stmt.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            if (con != null) try {
+                con.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
         return null;
     }
 
     public static Vector<Order> getOrdersFromDB(User owner){
-        return null;
+        PreparedStatement stmt = null;
+        Vector<Order> orders = new Vector<>();
+        Connection con = DBManager.getConnection();
+
+        String query = "SELECT * FROM ShopOrder WHERE owner = ?";
+
+        try{
+            stmt.setInt(1,owner.getId());
+            stmt = con.prepareStatement(query);
+            ResultSet rs = stmt.executeQuery();
+            while (rs.next()){
+                //TODO: Kanske verifiera användaren och inte anta att det är den användaren som skickades in.
+                orders.add(new OrderDB(rs.getInt("id"),owner,rs.getInt("status")));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }finally {
+            if (stmt != null) try {
+                stmt.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+
+            if(con != null) try {
+                con.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+
+
+        }
+        return orders;
     }
 
     public static void addOrderToDB(User owner){
@@ -28,17 +90,16 @@ public class OrderDB extends Order{
         PreparedStatement itemsStmt = null;
 
         Connection con = DBManager.getConnection();
-        String newOrderQuery = "INSERT INTO Order (owner,status) VALUES (?)";
-        String itemsToOrderQuery = "INSERT INTO OrderItem (order,item) VALUES (?,?)";
+        String newOrderQuery = "INSERT INTO ShopOrder (owner) VALUES (?)";
+        String itemsToOrderQuery = "INSERT INTO OrderItem (orderId,item) VALUES (?,?)";
 
 
         try {
             int auto_id = -1;
             con.setAutoCommit(false);
             orderStmt = con.prepareStatement(newOrderQuery, Statement.RETURN_GENERATED_KEYS);
-            //orderStmt.setInt(1,owner.getId());
-            orderStmt.setInt(1,3);
-            orderStmt.executeQuery();
+            orderStmt.setInt(1,owner.getId());
+            orderStmt.execute();
 
             ResultSet rs = orderStmt.getGeneratedKeys();
             rs.next();
@@ -46,13 +107,8 @@ public class OrderDB extends Order{
 
             itemsStmt = con.prepareStatement(itemsToOrderQuery);
 
-            /*for(ShoppingCartItem item : owner.getShoppingCart().getItems()){
-                itemsStmt.setInt(1,auto_id);
-                itemsStmt.setInt(2,5);
-                itemsStmt.execute();
-            }*/
-
-            for(int i = 0; i < 5; i++){
+            //TODO: get the shopingcart properly.
+            for(ShoppingCartItem item : owner.getShoppingCart().getItems()){
                 itemsStmt.setInt(1,auto_id);
                 itemsStmt.setInt(2,5);
                 itemsStmt.execute();
@@ -70,19 +126,79 @@ public class OrderDB extends Order{
             }
 
         }finally {
-            try{
-                if(newOrderQuery != null) orderStmt.close();
-                if (itemsToOrderQuery != null) itemsStmt.close();
+            if(newOrderQuery != null) try {
+                orderStmt.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            if (itemsToOrderQuery != null) try {
+                itemsStmt.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            if (con!=null) try {
                 con.setAutoCommit(true);
-            }catch (SQLException e){
-                System.err.println(e.getMessage());
+            } catch (SQLException e) {
+                e.printStackTrace();
             }
 
         }
 
     }
 
-    public static void updateOrder(int id){
+    public static void deleteOrder(int id){
+        PreparedStatement stmt = null;
 
+        Connection con = DBManager.getConnection();
+
+        String query = "DELETE FROM ShopOrder WHERE id=?";
+        try{
+            stmt.setInt(1,id);
+            stmt = con.prepareStatement(query);
+            stmt.execute();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }finally {
+            if (stmt != null) try {
+                stmt.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            if (con != null) try {
+                con.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void updateOrder(int id, User owner, int status){
+        PreparedStatement stmt = null;
+
+        Connection con = DBManager.getConnection();
+
+        String query = "UPDATE ShopOrder set owner = ?, status = ? WHERE id=?";
+
+        try {
+            stmt.setInt(1,owner.getId());
+            stmt.setInt(2,status);
+            stmt.setInt(3,id);
+            stmt = con.prepareStatement(query);
+            stmt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }finally {
+            if (stmt != null) try {
+                stmt.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            if (con != null) try {
+                con.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
