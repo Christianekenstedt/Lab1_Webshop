281c06aec3c1147a4fab01c48b3cdd1e5e74feb6
christianekenstedt <christian>
2016-09-28 11:47:02 +0200

    added controller

--- src/Controller/ServletController.java
+++ src/Controller/ServletController.java
@@ -1,9 +1,19 @@
 package Controller;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.*;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
 
 /**
- * Created by chris on 2016-09-28.
+ * Created by Christian Ekenstedt & Anton Wass on 2016-09-28.
  */
-public class ServletController {
+public class ServletController extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request,response);
+    }
 
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
 }
